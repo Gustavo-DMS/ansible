b4c14f4e5bdbbe76298454e9990812ce308bcd07
GustavoDMS <pipe.gus2014@gmail.com>
2024-11-18 22:28:07 -0300

    ansible-run.sh

---
+++ ansible-run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Install ansible
+sudo pacman -Syu
+sudo pacman -S git curl ansible
+
+## install ansible-galaxy roles
+ansible-galaxy install -r requirements.yml
+
+## Run ansible playbook
+ansible-playbook -i inventory main.yml -K --ask-vault-pass
