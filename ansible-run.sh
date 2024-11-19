429c783d313e095cf3a0ac21a08c87b6734f90e1
GustavoDMS <pipe.gus2014@gmail.com>
2024-11-19 00:49:01 -0300

    added -needed and install order

--- ansible-run.sh
+++ ansible-run.sh
@@ -2,7 +2,7 @@
 
 ## Install ansible
 sudo pacman -Syu
-sudo pacman -S git curl ansible
+sudo pacman -S --needed git curl ansible
 
 ## install ansible-galaxy roles
 ansible-galaxy install -r requirements.yml
